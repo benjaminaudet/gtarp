6d4af64a44e3e63a0a9b85dcf292bec639914fc3
Benjamin Audet <benjamin.audet@epitech.eu>
2017-04-16 16:26:15 +0200

    test to remove 1000$ when the player death

--- resources/[system]/baseevents/server.lua
+++ resources/[system]/baseevents/server.lua
@@ -6,6 +6,12 @@ RegisterServerEvent('baseevents:enteringAborted')
 RegisterServerEvent('baseevents:enteredVehicle')
 RegisterServerEvent('baseevents:leftVehicle')
 
+-- Loading MySQL Class
+require "resources/essentialmode/lib/MySQL"
+
+-- MySQL:open("IP", "databasname", "user", "password")
+MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "jujumanu78")
+
 AddEventHandler('baseevents:onPlayerKilled', function(killedBy, data)
 	local victim = source
 
@@ -16,4 +22,7 @@ AddEventHandler('baseevents:onPlayerDied', function(killedBy, pos)
 	local victim = source
 
 	RconLog({msgType = 'playerDied', victim = victim, attackerType = killedBy, pos = pos})
+
+	MySQL:executeQuery("UPDATE users SET `money`='@value' WHERE identifier = '@identifier'",
+	    {['@value'] = v.money - 1000, ['@identifier'] = GetPlayerIdentifiers(source)[1]})
 end)
