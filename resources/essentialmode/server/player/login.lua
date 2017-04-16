c09804b466f1204cb70c02d0f9079ea7bb57ce2a
Benjamin Audet <benjamin.audet@epitech.eu>
2017-04-16 16:56:09 +0200

    set DB password

--- resources/essentialmode/server/player/login.lua
+++ resources/essentialmode/server/player/login.lua
@@ -7,7 +7,7 @@
 require "resources/essentialmode/lib/MySQL"
 
 -- MySQL:open("IP", "databasname", "user", "password")
-MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "")
+MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "jujumanu78")
 
 function LoadUser(identifier, source, new)
 	local executed_query = MySQL:executeQuery("SELECT * FROM users WHERE identifier = '@name'", {['@name'] = identifier})
