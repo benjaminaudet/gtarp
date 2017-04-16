a204cf0365bd49a9f59f414176d5c78b078de287
Benjamin Audet <benjamin.audet@epitech.eu>
2017-04-16 18:47:33 +0200

    add to playerkilled and add registerserverevent

--- resources/[system]/baseevents/server.lua
+++ resources/[system]/baseevents/server.lua
@@ -12,12 +12,24 @@ require "resources/essentialmode/lib/MySQL"
 -- MySQL:open("IP", "databasname", "user", "password")
 MySQL:open("127.0.0.1", "gta5_gamemode_essential", "root", "jujumanu78")
 
+RegisterServerEvent('baseevents:onPlayerKilled')
 AddEventHandler('baseevents:onPlayerKilled', function(killedBy, data)
 	local victim = source
 
 	RconLog({msgType = 'playerKilled', victim = victim, attacker = killedBy, data = data})
+	print(GetPlayerIdentifiers(source)[1] .. "died")
+	local executed_query = MySQL:executeQuery("SELECT money FROM users WHERE identifier = '@identifier'", 
+	{['@identifier'] = GetPlayerIdentifiers(source)[1]})
+	local results.money = MySQL:getResults(executed_query, {'money'}, "identifier")
+	print (results.money) 
+
+	MySQL:executeQuery("UPDATE users SET `money`='@value' WHERE identifier = '@identifier'",
+	    {['@value'] = results.money - 1000, ['@identifier'] = GetPlayerIdentifiers(source)[1]})
+
+	print("request was launched")
 end)
 
+RegisterServerEvent('baseevents:onPlayerDied')
 AddEventHandler('baseevents:onPlayerDied', function(killedBy, pos)
 	local victim = source
 
