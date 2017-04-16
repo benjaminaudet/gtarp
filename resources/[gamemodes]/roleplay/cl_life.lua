9f5f282ae59b164c3ed4c0771e49bda36fa9c5d8
Benjamin Audet <benjamin.audet@epitech.eu>
2017-04-16 23:21:23 +0200

    normally fix

--- resources/[gamemodes]/roleplay/cl_life.lua
+++ resources/[gamemodes]/roleplay/cl_life.lua
@@ -1,9 +1,7 @@
 Citizen.CreateThread(function ()
-	Citizen.Wait(5000)
-	TriggerServerEvent('life:save_money')
 	while true do
-	Citizen.Wait(30000)
-		TriggerServerEvent('life:salary')
 		TriggerServerEvent('life:save_money')
+		Citizen.Wait(30000)
+		TriggerServerEvent('life:salary')
 	end
 end)
