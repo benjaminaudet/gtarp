fde7b4d49eaf13288c6b775b46dcf2b419d60bbc
Benjamin Audet <benjamin.audet@epitech.eu>
2017-04-16 21:51:27 +0200

    fix error on trigger event savepos

--- resources/[gamemodes]/roleplay/cl_life.lua
+++ resources/[gamemodes]/roleplay/cl_life.lua
@@ -2,6 +2,7 @@ Citizen.CreateThread(function ()
 	while true do
 	Citizen.Wait(360000)
 		TriggerServerEvent('life:salary')
-		TriggerServerEvent('life:savepos')
+		local pos = GetEntityCoords(GetPlayerPed(-1))
+		TriggerServerEvent('life:savepos', pos)
 	end
 end)
